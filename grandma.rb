875914152eb2cb91ea3427d82d6f5a31bd44d94a
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-24 05:14:48 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,11 @@
 def speak_to_grandma(sentence)
   if sentence == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif sentence.upcase == false 
+  elsif sentence != sentence.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else 
     return "NO, NOT SINCE 1938!"
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
