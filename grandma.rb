80d160724f3ea5e861772815ee174b5577baf4ba
Olga Rosas <olga.rosas@gmail.com>
2019-05-25 17:09:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif string != "I LOVE YOU GRANDMA!" 
+  elsif string != "I LOVE YOU GRANDMA!"
     return "NO, NOT SINCE 1938!"
-  else 
+  else
     return "HUH?! SPEAK UP, SONNY!"
 
 
@@ -13,7 +13,7 @@ def speak_to_grandma(string)
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
