80134485b745543c7d6164a1b6eb8d76ec52a2b7
Olga Rosas <olga.rosas@gmail.com>
2019-05-25 17:18:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(string)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
-  else
+  elseif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
 
 
